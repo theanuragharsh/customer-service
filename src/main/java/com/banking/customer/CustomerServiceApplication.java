48999c7e70a78ee109187b387cc9aa682e902032
theanuragharsh <nrgharsh@gmail.com>
2023-08-24 13:20:00 +0530

    configured docker-compose to include discovery-server for default profile in all services|| Included Zipkin ||

--- src/main/java/com/banking/customer/CustomerServiceApplication.java
+++ src/main/java/com/banking/customer/CustomerServiceApplication.java
@@ -2,12 +2,14 @@ package com.banking.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
+@RefreshScope
 @SpringBootApplication
 public class CustomerServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CustomerServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
 
 }
